585335658aef58730230625cd2eb5e9cd3ce2136
Albert Ramos <albertramos.dev@gmail.com>
2021-09-24 16:05:31 -0700

    Add exercise files

---
+++ docs1/ex2.rb
@@ -0,0 +1,5 @@
+# Title: while Loops
+
+# Locate the description of the while loop in the ruby documentation.
+
+# https://docs.ruby-lang.org/en/2.7.0/doc/syntax/control_expressions_rdoc.html#label-while+Loop
---
+++ docs1/ex3.rb
@@ -0,0 +1,6 @@
+# Title: Return Value of while
+
+# Using the ruby documentation, determine what value a while loop returns.
+
+# According to the documentation:
+# The result of a while loop is nil unless break is used to supply a value.
