5fd01e4b3e2a722b0942dab950989a95cdf02f00
Albert Ramos <albertramos.dev@gmail.com>
2021-09-24 18:36:40 -0700

    Add exercise files and delete txt file

---
+++ docs2/ex1.rb
@@ -0,0 +1,15 @@
+# How would you use String#upcase to create an uppercase version of the string "xyz"?
+
+str = "xyz"
+
+p str.upcase
+
+p str
+
+p str.upcase!
+
+p str
+
+# Or we can prepend the string to the method
+
+p "abc".upcase
