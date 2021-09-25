e08fea26e0d904f432bd5e0a52c10ec15f7069e3
Albert Ramos <albertramos.dev@gmail.com>
2021-09-24 19:04:15 -0700

    Looooong discussion on this one. Revisit later

---
+++ docs2/ex3.rb
@@ -0,0 +1,12 @@
+# Optional Arguments
+
+# Assume you have the following code:
+
+s = 'abc def ghi,jkl mno pqr,stu vwx yz'
+puts s.split.inspect
+puts s.split(',').inspect
+puts s.split(',', 2).inspect
+
+# What will each of the 3 puts statements print?
+
+# Looooong discussion on this one. Revisit later.
