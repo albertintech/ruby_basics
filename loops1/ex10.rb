0aa960af9a1b6e02aca86672a01f784c8c78fa9f
Albert Ramos <albertramos.dev@gmail.com>
2021-09-29 06:10:10 -0700

    Exercise done

---
+++ loops1/ex10.rb
@@ -0,0 +1,17 @@
+# Greet Your Friends
+
+# Your friends just showed up! Given the following array of names, use a for loop to greet each friend individually.
+
+heroes = ['Batman', 'Wonder Woman', 'Superman', 'Aquaman']
+
+for hero in heroes
+  puts "Hi, #{hero}!"
+end
+
+# Or LS solution
+
+friends = ['Sarah', 'John', 'Hannah', 'Dave']
+
+for friend in friends
+  puts "Hello, #{friend}!"
+end
