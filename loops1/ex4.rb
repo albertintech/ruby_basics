dbece566e89e10abe65824887961ba0e20651a85
Albert Ramos <albertramos.dev@gmail.com>
2021-09-29 05:09:10 -0700

    Exercise done

--- loops1/ex4.rb
+++ loops1/ex4.rb
@@ -3,7 +3,7 @@
 # Modify the code below so the loop stops iterating when the user inputs 'yes'.
 
 loop do
-  puts "Should I stop looping? Say 'yes' to stop."
+  puts "Should I stop looping? Say 'yes' to stop." # I added second sentence
   answer = gets.chomp
-  break if answer == "yes"
+  break if answer == "yes" # I added this line
 end
