3fa54d9400d6474f66e6c51b1d14a86e3f7f5528
Albert Ramos <albertramos.dev@gmail.com>
2021-09-29 12:03:05 -0700

    Exercise done

--- loops2/ex6.rb
+++ loops2/ex6.rb
@@ -1,1 +1,10 @@
 # Empty the Array
+
+# Given the array below, use loop to remove and print each name. Stop the loop once names doesn't contain any more elements.
+
+names = ['Sally', 'Joe', 'Lisa', 'Henry']
+
+loop do
+  puts names.shift # Shift removes first element and returns its value
+  break if names.empty? #Empty checks for any elements and returns a boolean
+end
