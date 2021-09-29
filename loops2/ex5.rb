4060b9c955859b03600924faef89965d0179ff87
Albert Ramos <albertramos.dev@gmail.com>
2021-09-29 11:18:17 -0700

    Exercise done

--- loops2/ex5.rb
+++ loops2/ex5.rb
@@ -6,6 +6,19 @@ numbers = []
 
 loop do
   puts 'Enter any number:'
-  input = gets.chomp.to_i
+  numbers << gets.chomp.to_i
+  break if numbers.size == 5
 end
 puts numbers
+
+# LS Solution
+# numbers = []
+#
+# loop do
+#   puts 'Enter any number:'
+#   input = gets.chomp.to_i
+#
+#   numbers.push(input)
+#   break if numbers.size == 5
+# end
+# puts numbers
