cd8564eed9db2214e849090744a87596685ca99a
Albert Ramos <albertramos.dev@gmail.com>
2021-09-30 14:04:26 -0700

    Exercise done

---
+++ user_input/ex1_repeat_after_me.rb
@@ -0,0 +1,5 @@
+# Write a program that asks the user to type something in, after which your program should simply display what was entered.
+
+puts "Say something and I will repeat it: "
+str = gets.chomp
+puts str
