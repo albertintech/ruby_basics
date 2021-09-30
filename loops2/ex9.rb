37f0f8ca394d1fa0db329a28aae9bf12f06403ab
Albert Ramos <albertramos.dev@gmail.com>
2021-09-30 09:21:47 -0700

    Add exercise

--- loops2/ex9.rb
+++ loops2/ex9.rb
@@ -1,1 +1,16 @@
 # First to Five
+
+# The following code increments number_a and number_b by either 0 or 1. loop is used so that the variables can be incremented more than once, however, break stops the loop after the first iteration. Use next to modify the code so that the loop iterates until either number_a or number_b equals 5. Print "5 was reached!" before breaking out of the loop.
+
+a = 0
+b = 0
+
+loop do
+  a += rand(2)
+  b += rand(2)
+
+  break
+end
+
+p a
+p b
