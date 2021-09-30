682054c745301689e6b833506aa307d3b3e88d7d
Albert Ramos <albertramos.dev@gmail.com>
2021-09-30 10:10:24 -0700

    Exercise done

--- loops2/ex3.rb
+++ loops2/ex3.rb
@@ -7,3 +7,18 @@
 # Use the following code to initialize process_the_loop. It will randomly assign the variable to either true or false.
 
 process_the_loop = [true, false].sample
+
+# I don't know, but questions that take a whole paragraph to explain kinda lose me. Especially when the payoff is paltry: "get more practice with loop syntax." That I can understand, but its buried here underneath a bunch of giberish.
+
+# Enough of my beef with this exercise...
+
+# LS solution:
+
+if process_the_loop
+  loop do
+    puts "The loop was processed!"
+    break
+  end
+else
+  puts "The loop wasn't processed!"
+end
