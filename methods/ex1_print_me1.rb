6280aec2f0f888b4800d6c63898187f706ddc4ac
Albert Ramos <albertramos.dev@gmail.com>
2021-10-01 12:37:45 -0700

    Exercise done

--- methods/ex1_print_me1.rb
+++ methods/ex1_print_me1.rb
@@ -1,4 +1,4 @@
-# Write a method named print_me that prints I'm printing within the method! when invoked as follows:
+# Write a method named print_me that prints 'I'm printing within the method!' when invoked as follows:
 
 def print_me
   puts "I'm printing within the method!"
