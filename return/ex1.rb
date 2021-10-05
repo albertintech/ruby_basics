3308e727920d91a4f648c0adc10949d7116dc1d1
Albert Ramos <albertramos.dev@gmail.com>
2021-10-04 18:27:30 -0700

    Exercise done

---
+++ return/ex1.rb
@@ -0,0 +1,12 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  return 'Breakfast'
+end
+
+puts meal
+
+# This will print 'Breakfast' if I am not mistaken.
+# I believe it will do so since puts is being used along with the method call.
+
+# Correct
