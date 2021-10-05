6ba435156eeca1282b2e4c6c2bfb3a20784126f0
Albert Ramos <albertramos.dev@gmail.com>
2021-10-04 18:33:56 -0700

    Exercise done

---
+++ return/ex3.rb
@@ -0,0 +1,14 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  return 'Breakfast'
+  'Dinner'
+end
+
+puts meal
+
+# This program will print 'Breakfast'
+# As soon as the method sees 'return', it will return the given string
+# The method doesn't reach the word 'Dinner'
+
+# Correct
