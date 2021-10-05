f5ffedc7fc9ab70abda24d521b404b14a07d41dd
Albert Ramos <albertramos.dev@gmail.com>
2021-10-05 09:58:33 -0700

    Exercise done

---
+++ strings/ex6_tricky_formatting.rb
@@ -0,0 +1,9 @@
+# Using the following code, capitalize the value of state then print the modified value. Note that state should have the modified value both before and after you print it.
+
+state = 'tExAs'
+
+# state = state.capitalize
+
+state.capitalize! # Should have used bang to mutate the caller
+
+puts state
