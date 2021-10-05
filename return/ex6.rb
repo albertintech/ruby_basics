e2471f029cba0e8ae9e162e30e860955cea4148e
Albert Ramos <albertramos.dev@gmail.com>
2021-10-04 18:48:23 -0700

    Exercise done

---
+++ return/ex6.rb
@@ -0,0 +1,13 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  return 'Breakfast'
+  'Dinner'
+  puts 'Dinner'
+end
+
+puts meal
+
+# >> 'Breakfast'
+
+# Again, the return keyword forces the method to exit with the given return value
