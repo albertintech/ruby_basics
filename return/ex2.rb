628b8f2fac8f5a5b717ae655b2f782c222def52d
Albert Ramos <albertramos.dev@gmail.com>
2021-10-04 18:29:40 -0700

    Exercise done

---
+++ return/ex2.rb
@@ -0,0 +1,11 @@
+# What will the following code print? Why? Don't run it until you've attempted to answer.
+
+def meal
+  'Evening'
+end
+
+puts meal
+
+# This should also return the string since it is the last line in the method and it is called with puts
+
+# Correct
