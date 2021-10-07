7cc970689e3e10ac7ef6b8b75360bed01cd8bf83
Albert Ramos <albertramos.dev@gmail.com>
2021-10-07 11:38:50 -0700

    Exercise done

---
+++ hashes/ex10_which_collection.rb
@@ -0,0 +1,18 @@
+# Rewrite car as a nested array containing the same key-value pairs.
+
+car = {
+  type:  'sedan',
+  color: 'blue',
+  year:  2003
+}
+
+# car[['type', 'sedan'], ['color','blue'], ['year', 2003]]
+
+# Almost, but not quite. We can still use symbols and make the assignment:
+
+car = [[:type, 'sedan'], [:color, 'blue'], [:year, 2003]]
+
+# Discussion
+# Transferring this data from a hash to a nested array is somewhat trivial. However, it's important to understand the strengths and weaknesses of hashes and arrays. The data in this exercise is better suited for a hash because the values require labels. With an array, you're not able to label specific values.
+
+# Arrays shine, however, when dealing with ordered lists. Although, as of recent Ruby versions, hashes are ordered, arrays are widely preferred for ordered data.
