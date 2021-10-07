e1ab834a7706e1dbe0878715d998f57c1f946984
Albert Ramos <albertramos.dev@gmail.com>
2021-10-07 10:48:54 -0700

    Exercise done

---
+++ hashes/ex2_adding_the_year.rb
@@ -0,0 +1,14 @@
+# Using the code below, add the key :year and the value 2003 to car.
+
+car = {
+  type:    'sedan',
+  color:   'blue',
+  mileage: 80_000
+}
+
+# car[year: 2003] Wrong!
+
+car[:year] = 2003 # That's better
+
+# hash_name[exisiting or new symbol] = new value
+ 
