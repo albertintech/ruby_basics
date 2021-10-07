3c6b96b40fa0d0481197568c77bf96990b4f6076
Albert Ramos <albertramos.dev@gmail.com>
2021-10-07 10:28:08 -0700

    Exercise done

---
+++ arrays/ex9_favorite_number2.rb
@@ -0,0 +1,7 @@
+# In the code below, a nested array containing three groups of names and numbers is assigned to favorites.
+
+favorites = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+new = favorites.flatten
+
+puts "Here ya go, as ordered #{new}"
